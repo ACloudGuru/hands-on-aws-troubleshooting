5686d6853032743c6a20def5d6e3e0653040c854
Georgios Serafeim <g-ser@users.noreply.github.com>
2023-07-12 20:44:39 +0200

    Fix lambda_function.py

--- Building_and_Troubleshooting_a_Serverless_Web_Application/lambda_function.py
+++ Building_and_Troubleshooting_a_Serverless_Web_Application/lambda_function.py
@@ -1,37 +1,33 @@
+import json
+import boto3
+import random
+
+client = boto3.client('dynamodb')
+
 def lambda_handler(event, context):
-    print("In lambda handler")
-    import boto3
-    import botocore
-    import random
-    import json
-    from boto3.dynamodb.conditions import Key, Attr
-   
 
-    fortid = (random.randint(1,16))
-    dynamodb = boto3.resource("dynamodb")
-    table = dynamodb.Table('fortunes')
-
-    response = table.get_item(
-        Key={
-            'fort_id': fortid
+  data = client.get_item(
+    TableName='fortunes',
+    Key={
+        'fort_id': {
+          'N': str(random.randint(1,16))
+        }
         },
         ProjectionExpression='fortune'
-        
     )
-    
-    json_string = json.dumps(response)
-    resp_dict=json.loads(json_string)
-    fort_string=json.dumps(resp_dict['Item'])
-    fort_dict=json.loads(fort_string)
 
-    resp = {
-         "statusCode": 200,
-        "headers": {
-            "Access-Control-Allow-Origin": "*",
-        },
-        "body": json.dumps(fort_dict['fortune'])
-    }
+  json_string = json.dumps(data)
+  resp_dict=json.loads(json_string)
+  fort_string=json.dumps(resp_dict['Item'])
+  fort_dict=json.loads(json.dumps(json.loads(fort_string)['fortune']))
 
-    return resp
-    
+  response = {
+      'statusCode': 200,
+      'headers': {
+        'Access-Control-Allow-Origin': '*',
+      },
+      'body': json.dumps(fort_dict['S'])
 
+  }
+  
+  return response
